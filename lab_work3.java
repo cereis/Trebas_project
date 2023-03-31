1e64a3f820fa33ea4b22487d0652975e1981ad3e
cereis <124637719+cereis@users.noreply.github.com>
2023-03-31 19:41:11 -0400

    Add files via upload

---
+++ lab_work3.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author cecib
+ */
+import java.util.Scanner;
+
+public class lab_work3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the coupon/interest payment: ");
+        double c = scanner.nextDouble();
+        System.out.print("Enter the face value: ");
+        double f = scanner.nextDouble();
+        System.out.print("Enter the price: ");
+        double p = scanner.nextDouble();
+        System.out.print("Enter the years to maturity: ");
+        double n = scanner.nextDouble();
+
+        double ytm = (c + ((f - p) / n)) / ((f + p) / 2);
+        System.out.printf("The approximate Yield to Maturity is: %.2f%%\n", ytm * 100);
+    }
+}
